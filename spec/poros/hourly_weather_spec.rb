41c381da0f2a31c7ed5c59ea2929980ebeb729dd
Conor Barthel <conor.barthel@gmail.com>
2022-04-23 11:20:18 -0600

    Test: HourlyWeather Poro

---
+++ spec/poros/hourly_weather_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe HourlyWeather do
+  it "can make a weather object" do
+    data = {
+        dt: 1650678092,
+        temp: 250.41,
+        weather: [
+            {
+                description: "overcast clouds",
+                icon: "04d"
+            }
+        ]
+    }
+
+    current_weather = HourlyWeather.new(data)
+    expect(current_weather.datetime).to eq("19:41:32")
+    expect(current_weather.temperature).to eq(250.41)
+    expect(current_weather.conditions).to eq("overcast clouds")
+    expect(current_weather.icon).to eq("04d")
+  end
+end
