0a1dbceca06dae8e40dee06efe4b8f21d45c6c2d
Conor Barthel <conor.barthel@gmail.com>
2022-04-23 11:14:40 -0600

    Feat: Daily weather poro

--- app/poros/daily_weather.rb
+++ app/poros/daily_weather.rb
@@ -1,21 +1,18 @@
-class CurrentWeather
+class DailyWeather
   attr_reader :datetime,
               :sunrise,
               :sunset,
-              :temperature,
-              :feels_like,
+              :max_temp,
+              :min_temp,
               :conditions,
               :icon
 
   def initialize(data)
-     @datetime = Time.at(data[:dt]).to_s
+     @datetime = Time.at(data[:dt]).to_s.split(" ").shift
      @sunrise = Time.at(data[:sunrise]).to_s
      @sunset = Time.at(data[:sunset]).to_s
-     @temperature = data[:temp]
-     @feels_like = data[:feels_like]
-     @humidity = data[:humidity]
-     @uvi = data[:uvi]
-     @visibility = data[:visibility]
+     @max_temp = data[:temp][:max]
+     @min_temp = data[:temp][:min]
      @conditions = data[:weather].first[:description]
      @icon = data[:weather].first[:icon]
   end
