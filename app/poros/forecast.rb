18cee5422d81a6e8122b2a8803f72e83660c0f51
Conor Barthel <conor.barthel@gmail.com>
2022-04-24 09:25:31 -0600

    Test: forcast request spec

--- app/poros/forecast.rb
+++ app/poros/forecast.rb
@@ -2,6 +2,8 @@ class Forecast
   attr_reader :daily_weather, :hourly_weather, :current_weather
 
   def initialize(current_weather, hourly_weather, daily_weather)
+    @id = nil
+    @type = 'forecast'
     @current_weather = current_weather
     @hourly_weather = hourly_weather
     @daily_weather = daily_weather
