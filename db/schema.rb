98c39377cb86a9ce5536d95b8fb3485ae339bda2
Conor Barthel <conor.barthel@gmail.com>
2022-04-25 13:40:21 -0600

    Feat: create user class

--- db/schema.rb
+++ db/schema.rb
@@ -10,26 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_22_223628) do
+ActiveRecord::Schema.define(version: 2022_04_25_193907) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "weathers", force: :cascade do |t|
-    t.datetime "sunrise"
-    t.datetime "sunset"
-    t.float "temperature"
-    t.float "feelsLike"
-    t.integer "humidity"
-    t.integer "uvi"
-    t.integer "visability"
-    t.string "conditions"
-    t.string "icon"
-    t.float "maxTemp"
-    t.float "minTemp"
-    t.datetime "time"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
   end
 
 end
