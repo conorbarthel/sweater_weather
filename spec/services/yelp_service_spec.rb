65418a26fca8a8ea70ce7104e62c442fd87efcd0
Conor Barthel <conor.barthel@gmail.com>
2022-04-25 10:26:25 -0600

    Test: yelp service test

---
+++ spec/services/yelp_service_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe YelpService do
+  describe '#conn' do
+    it "returns json response" do
+      response = YelpService.conn
+
+      expect(response).to be_a Faraday::Connection
+    end
+  end
+  
+  describe '#get_restaurant' do
+    it 'will return JSON of a restaurant', :vcr do
+      restaurant_details = YelpService.get_coords('Denver,CO')
+      expect(restaurant_details).to be_a Hash
+      expect(restaurant_details[:businesses]).to be_a Array
+      expect(restaurant_details[:businesses].first).to be_a Hash
+      expect(restaurant_details[:businesses].first).to include(:name, :location)
+      expect(restaurant_details[:businesses].first[:locations]).to be_a Hash
+      expect(restaurant_details[:businesses].first[:locations].first).to be_a Array
+      expect(restaurant_details[:businesses].first[:locations].first).to include(:display_address)
+    end
+  end
+end
