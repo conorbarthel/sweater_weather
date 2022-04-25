c9975530472b1cd5822cdf51ddcea3aaa5b46863
Conor Barthel <conor.barthel@gmail.com>
2022-04-25 11:24:27 -0600

    Feat: YelpService #get_restaurant

--- spec/services/yelp_service_spec.rb
+++ spec/services/yelp_service_spec.rb
@@ -12,13 +12,13 @@ RSpec.describe YelpService do
   describe '#get_restaurant' do
     it 'will return JSON of a restaurant', :vcr do
       restaurant_details = YelpService.get_restaurant('pueblo,co', 'chinese')
+      #binding.pry
       expect(restaurant_details).to be_a Hash
       expect(restaurant_details[:businesses]).to be_a Array
       expect(restaurant_details[:businesses].first).to be_a Hash
       expect(restaurant_details[:businesses].first).to include(:name, :location)
-      expect(restaurant_details[:businesses].first[:locations]).to be_a Hash
-      expect(restaurant_details[:businesses].first[:locations].first).to be_a Array
-      expect(restaurant_details[:businesses].first[:locations].first).to include(:display_address)
+      expect(restaurant_details[:businesses].first[:location]).to be_a Hash
+      expect(restaurant_details[:businesses].first[:location][:display_address]).to be_a Array
     end
   end
 end
