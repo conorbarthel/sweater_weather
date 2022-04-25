82311d5ee672fd3bab719476990d4b69470d32d6
Conor Barthel <conor.barthel@gmail.com>
2022-04-25 14:59:11 -0600

    Feat: redid user migrations to take in auth token

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_25_203701) do
+ActiveRecord::Schema.define(version: 2022_04_25_205755) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,10 +18,11 @@ ActiveRecord::Schema.define(version: 2022_04_25_203701) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "api_key"
-    t.string "email"
+    t.index ["api_key"], name: "index_users_on_api_key", unique: true
   end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,6 +7,6 @@ describe User, type: :model do
     it {should validate_presence_of(:password)}
     it {should validate_presence_of(:email)}
     it {should validate_uniqueness_of(:email)}
-    
+    it {should have_secure_password}
   end
 end
