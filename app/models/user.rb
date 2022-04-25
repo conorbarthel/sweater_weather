cd284f1e391cd5f28487c50663e580c710111458
Conor Barthel <conor.barthel@gmail.com>
2022-04-25 14:43:41 -0600

    Feat: add validations to users and email column

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,18 @@
 class User < ApplicationRecord
   validates :username, uniqueness: true, presence: true
   validates_presence_of :password, require: true
-
+  validates :email, uniqueness: true, presence: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  
   has_secure_password
+
+  has_secure_token :api_key
+
+  before_create :set_api_key
+
+  private
+
+  def set_api_key
+    self.api_key = SecureRandom.hex(13)
+  end
 end
