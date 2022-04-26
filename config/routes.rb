6b2f5dc02395673f592eb36adfc01445c93eedd9
Conor Barthel <conor.barthel@gmail.com>
2022-04-25 20:11:01 -0600

    Feat: Route for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
       resources :forecast, only: [:index]
       resources :backgrounds, only: [:index]
       resources :munchies, only: [:index]
-      resources :users
+      resources :users, only: [:create]
+      resources :sessions, only: [:create]
     end
   end
 end
