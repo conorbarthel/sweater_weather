341ce94073924f455c6e870531480aa0069b2846
Conor Barthel <conor.barthel@gmail.com>
2022-04-26 10:52:03 -0600

    Test: Roadtrip request

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::MunchiesController < ApplicationController
     destination = params[:destination]
     coordinates = MapFacade.get_coords(destination)
     forecast = WeatherService.get_weather(coordinates.lat, coordinates.lng)
-    destination_data = MapService.get_route(destination, params[:start])
+    destination_data = MapService.get_route(params[:start], destination)
     restaurant = YelpService.get_restaurant(destination, params[:food])
     munchie = Munchie.new(destination, restaurant, destination_data, forecast[:current])
     render(json: MunchieSerializer.new(munchie))
