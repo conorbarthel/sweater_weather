a6ed23b056b73e4be6224c33c5272d73f75d42ce
Conor Barthel <conor.barthel@gmail.com>
2022-04-26 11:09:29 -0600

    Test: MapFacade #valid?

---
+++ app/controllers/api/v1/roadtrip_controller.rb
@@ -0,0 +1,13 @@
+class Api::V1::RoadTripsController < ApplicationController
+  def create
+    user = User.find_by(api_key: params[:api_key])
+    if user.present? && MapFacade.valid?(params[:origin], params[:destination])
+      render json: RoadTripSerializer.new(user), status: 201
+    elsif user.present? == false
+      render json: { error: "Unauthorized", status: 401 }
+    elsif MapFacade.valid?(params[:origin], params[:destination]) == false
+      render json: { error: "Invalid RoadTrip", status: 400 }
+    end
+  end
+
+end
